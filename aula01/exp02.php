41b5170454bf0b9cfc6b194de482fa71ff5ec2a5
raifran_guimaraes <145464430+Raifra@users.noreply.github.com>
2023-10-17 11:20:58 -0300

    Add files via upload

---
+++ aula01/exp02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp02</title>
+</head>
+<body>
+    <?php
+    echo 'apresendendo sobre php <br>';
+    echo 'testando o codigo <br>';
+    echo "vamos ai <br>";
+    echo 'html'; echo '<br>';
+    echo 'teste3 ' . 'concatenacao' . '<br>';
+    echo 'meu nome ' . ' maria ' . '<br>';
+    ?>
+    
+</body>
+</html>
