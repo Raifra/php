3b09b3e6334bf60860d71ae38a2efc5178d1b751
raifran_guimaraes <145464430+Raifra@users.noreply.github.com>
2023-11-07 22:26:07 -0300

    Add files via upload

---
+++ aula02/exp01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp01.css">
+</head>
+<style>
+    
+
+</style>
+<body>
+    <h1 style="background-color: red;">Titulo inicial</h1>
+    <p style= "background-color: black">Segundo raifran</p>
+    <h2>Aprendendo o css</h2>
+    <p id="p2">Segundo o filosofo Raifran tudo e bom</p>
+
+
+    
+</body>
+</html>
---
+++ aula02/exp02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp02.css">
+</head>
+<body>
+    <h1>Titulo principal</h1>
+    <p>Oração e todo periodo com verdo</p>
+    <h2>Frase</h2>
+    <p id="p2">E todo periodo sem verdo</p>
+
+    <p class="stilo">Qulquer paragrafo</p>
+    <h2 class="stilo"> segundo</h2>
+    <p class="stilo">Segundo o mesmo</p>
+   
+    
+
+
+</body>
+</html>
